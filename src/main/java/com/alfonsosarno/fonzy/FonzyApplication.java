772d2c028ca37dda0468dc3b1f76f6e5f2f89818
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-03 09:17:25 +0100

    Initial commit

---
+++ src/main/java/com/alfonsosarno/fonzy/FonzyApplication.java
@@ -0,0 +1,13 @@
+package com.alfonsosarno.fonzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfonsosarno/fonzy/FonzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfonsosarno.fonzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
