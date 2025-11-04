38d9ab8b5bd4e1dacf6fd43aea277fb73facd9d1
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-04 16:38:25 +0100

    aggiunte exceptions

---
+++ src/main/java/com/alfonsosarno/fonzy/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.alfonsosarno.fonzy.exceptions;
+
+import java.util.UUID;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(UUID uuid) {
+        super("l'abbonamento con id: " + uuid + " non è stato trovato");
+    }
+}
