1e9eb967da1650e26bbc1fd6a8cb8c31c1586199
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-06 10:14:01 +0100

    not found e validation exception

--- src/main/java/com/alfonsosarno/fonzy/exceptions/NotFoundException.java
+++ src/main/java/com/alfonsosarno/fonzy/exceptions/NotFoundException.java
@@ -5,5 +5,12 @@ import java.util.UUID;
 public class NotFoundException extends RuntimeException {
     public NotFoundException(UUID uuid) {
         super("l'abbonamento con id: " + uuid + " non è stato trovato");
+
+
+
     }
+        public NotFoundException(String message){
+            super(message);
+        }
 }
+
