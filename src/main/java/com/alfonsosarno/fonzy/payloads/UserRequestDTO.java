b2ca6c49df19e27eb40e24a9b8cf8650fc38070b
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-06 10:16:00 +0100

    add validation in userDTO

--- src/main/java/com/alfonsosarno/fonzy/payloads/UserRequestDTO.java
+++ src/main/java/com/alfonsosarno/fonzy/payloads/UserRequestDTO.java
@@ -1,4 +1,25 @@
 package com.alfonsosarno.fonzy.payloads;
 
-public record UserRequestDTO(String username, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDTO(
+
+        @NotBlank(message = "Username obbligatorio")
+        @Size(min = 3, max = 15, message = "lo username deve avere una lunghezza compresa tra 3 e 15 caratteri")
+        String username,
+
+        @NotBlank(message = "L'email è obbligatoria!")
+        @Email(message = "L'email inserito non è corretto")
+        String email,
+
+        @NotBlank(message = "La password è obbligatoria!")
+        @Size(min = 6, message = "La password deve avere minimo 6 caratteri")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{6,}$",
+                message = "La password deve contenere almeno una lettera maiuscola, una minuscola, un numero e un carattere speciale, e deve essere lunga almeno 6 caratteri."
+        )
+        String password) {
 }
