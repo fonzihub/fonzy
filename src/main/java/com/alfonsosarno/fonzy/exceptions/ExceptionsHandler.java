a54801c3ac2d2774ae8afd8637c5252f7824f7ba
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-06 10:13:44 +0100

    exception handler

---
+++ src/main/java/com/alfonsosarno/fonzy/exceptions/ExceptionsHandler.java
@@ -0,0 +1,48 @@
+package com.alfonsosarno.fonzy.exceptions;
+
+import com.alfonsosarno.fonzy.payloads.ErrorsDTO;
+import com.alfonsosarno.fonzy.payloads.ErrorsWithListDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+public class ExceptionsHandler {
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsWithListDTO handleValidationErrors(ValidationException ex) {
+        return new ErrorsWithListDTO(ex.getMessage(), LocalDateTime.now(), ex.getErrorsMessages());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+    public ErrorsDTO handleUnauthorizedException(UnauthorizedException ex) {
+        return new ErrorsDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(BadRequestException.class) // Tra le parentesi indico quale eccezione dovrà gestire questo metodo
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+    public ErrorsDTO handleBadRequest(BadRequestException ex) {
+        return new ErrorsDTO(ex.getMessage(), LocalDateTime.now());
+    }
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorsDTO handleForbidden(AuthorizationDeniedException ex) {
+        return new ErrorsDTO("Non hai i permessi per accedere!", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorsDTO handleNotFound(NotFoundException ex) {
+        return new ErrorsDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+    @ExceptionHandler(Exception.class)
+    public ErrorsDTO handleServerError(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsDTO("C'è stato un errore generico, giuro che lo risolveremo presto!", LocalDateTime.now());
+    }
+}
