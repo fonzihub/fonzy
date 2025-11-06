a2a2513653146cffa1665f2b6bbfffe3742b0a93
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-06 10:15:10 +0100

    jwt filter autenticazione e autorizzazione

---
+++ src/main/java/com/alfonsosarno/fonzy/security/JWTFilter.java
@@ -0,0 +1,62 @@
+package com.alfonsosarno.fonzy.security;
+
+import com.alfonsosarno.fonzy.entities.User;
+import com.alfonsosarno.fonzy.exceptions.UnauthorizedException;
+import com.alfonsosarno.fonzy.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTTools jwtTools;
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+//       ----------------AUTENTICAZIONE-----------
+
+        String authHeader = request.getHeader("Authorization");
+if(authHeader == null || !authHeader.startsWith("Bearer "))
+    throw new UnauthorizedException("Inserire il token nel formato giusto!");
+
+String accessToken = authHeader.replace("Bearer ", "");
+
+jwtTools.verifyToken(accessToken);
+
+
+// --------------- AUTORIZAZZIONE -------------------
+
+        UUID userId = jwtTools.extractIdFromToken(accessToken);
+        User found = userService.findById(userId);
+
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(found,null, found.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+
+
+
+
+    }
+        @Override
+        protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+            return new AntPathMatcher().match("/auth/**", request.getServletPath());
+        }
+}
