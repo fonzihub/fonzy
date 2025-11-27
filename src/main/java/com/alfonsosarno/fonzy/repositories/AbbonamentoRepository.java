623e51b02961043bc1b59ae0be31ece7c2262007
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-27 18:01:27 +0100

    Add custom query methods to AbbonamentoRepository

--- src/main/java/com/alfonsosarno/fonzy/repositories/AbbonamentoRepository.java
+++ src/main/java/com/alfonsosarno/fonzy/repositories/AbbonamentoRepository.java
@@ -1,11 +1,17 @@
 package com.alfonsosarno.fonzy.repositories;
 
 import com.alfonsosarno.fonzy.entities.Abbonamento;
+import com.alfonsosarno.fonzy.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AbbonamentoRepository extends JpaRepository<Abbonamento, UUID> {
+    List<Abbonamento> findByDataRinnovoBetween(LocalDate start, LocalDate end);
+    List<Abbonamento> findByUser(User user);
+    List<Abbonamento> findByDataScadenza(LocalDate data);
 }
