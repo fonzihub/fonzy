810b277cef2e8e9aa37dd3aed610aaf3c419c7d8
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-27 17:59:30 +0100

    Integrate JWT filter and update CORS origins

--- src/main/java/com/alfonsosarno/fonzy/security/SecurityConfig.java
+++ src/main/java/com/alfonsosarno/fonzy/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.alfonsosarno.fonzy.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,12 +23,30 @@ import java.util.List;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+
+    private final JWTFilter jwtFilter;
+
+
+    @Autowired
+    public SecurityConfig(@Lazy JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain secFilChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.formLogin(form -> form.disable());
         httpSecurity.csrf(cs -> cs.disable());
         httpSecurity.sessionManagement(se -> se.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+        httpSecurity.authorizeHttpRequests(req -> req
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        // Aggiungi il filtro JWT
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.build();
     }
@@ -38,7 +59,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of( "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5176",
+                "http://localhost:5175",
+                "http://localhost:5174",
+                "http://localhost:5173"
+        ));
 
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
