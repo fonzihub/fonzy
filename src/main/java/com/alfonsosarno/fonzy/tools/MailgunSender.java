991b447e28ecbb8b513d5bd93d6215d3f8b31a62
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-27 17:53:37 +0100

    add sendingbillingEmail

--- src/main/java/com/alfonsosarno/fonzy/tools/MailgunSender.java
+++ src/main/java/com/alfonsosarno/fonzy/tools/MailgunSender.java
@@ -33,17 +33,21 @@ public class MailgunSender {
         System.out.println(response.getBody());
     }
 
-    public void sendBillingEmail(User recipient, Abbonamento abb) {
+    public void sendBillingEmail(User recipient, Abbonamento abbonamento) {
+        String testo = "Ciao " + recipient.getUsername() +
+                ", il tuo abbonamento \"" + abbonamento.getNomeAbbonamento() + "\" scadrà tra 3 giorni, il " +
+                abbonamento.getDataRinnovo() + ". Ti invitiamo a rinnovarlo per continuare a usare il servizio.";
+
         HttpResponse<JsonNode> response = Unirest.post("https://api.mailgun.net/v3/" + this.domain + "/messages")
-                .basicAuth("api",this.apiKey)
-                .queryString("from","brad@sandbox244a1f5e40eb4904980a949ae26268c6.mailgun.org")
-                .queryString("to",recipient.getEmail())
-                .queryString("subject","Registrazione completata")
-                .queryString("text","Ciao, " + recipient.getUsername() + ", il tuo abbonamneto a " +
-                        abb.getCatalogo().getName() + " scade il " + abb.getDataRinnovo() + ". Ricordati di rinnovarlo! ")
+                .basicAuth("api", this.apiKey)
+                .queryString("from", "alfonsosarno1@icloud.com")
+                .queryString("to", recipient.getEmail())
+                .queryString("subject", "Abbonamento in scadenza")
+                .queryString("text", testo)
                 .asJson();
 
         System.out.println(response.getBody());
     }
+
 }
 
