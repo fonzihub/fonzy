08093e32b5c0f801dbd8559bed08fe5e075de086
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-11-27 17:55:25 +0100

    auth service

--- src/main/java/com/alfonsosarno/fonzy/services/AuthService.java
+++ src/main/java/com/alfonsosarno/fonzy/services/AuthService.java
@@ -10,31 +10,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
     @Autowired
     private UserService userService;
+
     @Autowired
     private JWTTools jwtTools;
+
     @Autowired
     private PasswordEncoder bcrypt;
 
-
     public String checkCredentialsAndGenerateToken(LoginDTO body) {
-        //  Controllo credenziali
-        // Controllo nel DB se esiste un utente con quell'indirizzo email (fornito nel body)
+
         User found = this.userService.findByEmail(body.email());
 
-        //  Se esiste verifico che la sua password corrisponda a quella del body
-        //  Se una delle 2 verifiche non va a buon fine --> 401
         if (bcrypt.matches(body.password(), found.getPassword())) {
-            //  Se credenziali OK --> Genero un access token
-            // Ritorno il token
             return jwtTools.createToken(found);
         } else {
             throw new UnauthorizedException("Credenziali errate!");
         }
-
     }
-
-
-
 }
